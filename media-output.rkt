cafa10363380628624e715d9c1e610eaf5db17d3
hul <1013644379@qq.com>
2021-12-21 17:49:10 +0800

    add

--- media-output.rkt
+++ media-output.rkt
@@ -9,13 +9,29 @@
 (define (display-media type content)
   (cond
     [(bytes? content)
-      (let ([filename (write-binary-file content)])
-        (display (format "[s:~A:id=~A]" type filename)))]
+     (define (write-binary-file bytes)
+       (define filename (real->int (current-inexact-milliseconds)))
+       (define out (open-output-file (format "~A/~A" temp-dir filename) #:mode 'binary))
+       (write-bytes bytes out)
+       (close-output-port out)
+       filename)
+     (let ([filename (write-binary-file content)])
+       (display (format "[s:~A:id=~A]" type filename)))]
     [(string? content) (display (format "[s:~A:url=~A]" type content))]))
 
-(define (write-binary-file bytes)
-  (define filename (real->int (current-inexact-milliseconds)))
-  (define out (open-output-file (format "~A/~A" temp-dir filename) #:mode 'binary))
-  (write-bytes bytes out)
-  (close-output-port out)
-  filename)
+
+(require net/url
+         'http-util)
+
+(define (display-image/redirect url)
+  (define-values (status headers in)
+    (http-sendrecv/url (string->url url)))
+  (cond
+    [(string-contains? (bytes->string/utf-8 status) "302")
+     (define location (get-header-value #"Location" headers))
+     (when location
+       (set! location (string-replace location " " "%20"))
+       (display-image location))]
+    [else (error (format "[~a]的请求未响应重定向,请尝试使用 display-image" url))]))
+
+
