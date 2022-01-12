5e026b1b25a4b324a8dd1da9d357a24c4a7422cd
hul <1013644379@qq.com>
2022-01-12 15:43:24 +0800

    update: 输出图像音频

--- media-output.rkt
+++ media-output.rkt
@@ -1,23 +1,9 @@
-(define temp-dir "x")
+(define (display-image url)
+  (__eval-output-add-image #:url url))
 
-(define (display-image image)
-  (display-media "image" image))
 
-(define (display-audio audio)
-  (display-media "audio" audio))
-
-(define (display-media type content)
-  (cond
-    [(bytes? content)
-     (define (write-binary-file bytes)
-       (define filename (real->int (current-inexact-milliseconds)))
-       (define out (open-output-file (format "~A/~A" temp-dir filename) #:mode 'binary))
-       (write-bytes bytes out)
-       (close-output-port out)
-       filename)
-     (let ([filename (write-binary-file content)])
-       (display (format "[s:~A:id=~A]" type filename)))]
-    [(string? content) (display (format "[s:~A:url=~A]" type content))]))
+(define (display-audio url)
+  (__eval-output-add-audio #:url url))
 
 
 (require net/url
